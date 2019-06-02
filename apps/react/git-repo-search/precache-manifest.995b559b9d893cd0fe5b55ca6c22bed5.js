e497d3c7d56975bb5d097ee182a39ebe97da7156
vipinkrishna <vipinkrishna.in@gmail.com>
2019-06-03 04:07:53 +0530

    git repo search app

---
+++ apps/react/git-repo-search/precache-manifest.995b559b9d893cd0fe5b55ca6c22bed5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a4eb28dc97f1e3e791e634a7065549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2da6b8eac6e68c9e9b3",
+    "url": "/static/css/main.e0a53c9e.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "f2da6b8eac6e68c9e9b3",
+    "url": "/static/js/main.9961f23b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
