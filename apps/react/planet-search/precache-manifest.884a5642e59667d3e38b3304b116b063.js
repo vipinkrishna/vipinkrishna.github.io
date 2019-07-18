ec38d9a8bb2b3016a805e149b317fd8f0cf80760
vipinkrishna <vipinkrishna.in@gmail.com>
2019-07-18 12:33:03 +0530

    planet-search (STAR WARS API APP - https://swapi.co/) - vipinkrishna (REACT, REDUX, THUNK)

---
+++ apps/react/planet-search/precache-manifest.884a5642e59667d3e38b3304b116b063.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa57d6fce005fc8776c76a4a3e5688e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "843b9e3c55e12b2b29ae",
+    "url": "/static/css/main.35729630.chunk.css"
+  },
+  {
+    "revision": "fce874d578e36c3361d6",
+    "url": "/static/js/2.8c069d4f.chunk.js"
+  },
+  {
+    "revision": "843b9e3c55e12b2b29ae",
+    "url": "/static/js/main.fc2e97f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
